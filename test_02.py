82f160cd13932e617282d9490beab919aecc62d7
adam-paul <adam-paul@users.noreply.github.com>
2016-04-29 13:41:37 -0700

    Create test_02.py

---
+++ test_02.py
@@ -0,0 +1,11 @@
+# Import the sys package to return exit codes
+import sys
+
+def try_exit(i=0):
+   if i == 0:
+       sys.exit(0)
+   else:
+       sys.exit(1)
+
+print 'This should pass!'
+try_exit(i=0)
