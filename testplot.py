d1cf779d10a1756dcc9d0ac3fddd5d48dce941a9
adampaul <adampaul@uvic.ca>
2016-05-02 15:34:59 -0700

    New python script to test plotting with travis

---
+++ testplot.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+def plotter(x, y):
+    x = np.asarray(x)
+    y = np.asarray(y)
+
+    plt.plot(x, y)
+    plt.savefig('testplot.png')
+
+x = [1, 2, 3, 4, 5]
+y = [1, 2, 3, 4, 5]
+
+plotter(x, y)
